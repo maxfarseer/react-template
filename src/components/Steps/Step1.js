253440c853df5add041bd1da51c34be3d23df148
Andrey Kalashnik <andreykalashnik@Andreys-MacBook-Pro.local>
2017-03-14 13:26:29 +0100

    added new fields

--- src/components/Steps/Step1.js
+++ src/components/Steps/Step1.js
@@ -4,29 +4,80 @@ export default class Step1 extends Component {
     super(props)
     this.onInputChange = this.onInputChange.bind(this)
     this.state = {
-      test: '',
+      name: '',
+      country: '',
+      postCode: '',
+      state: '',
+      city: '',
+      street: '',
     }
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.data.name) {
-      this.setState({ test: nextProps.data.name })
+      this.setState({
+        name: nextProps.data.name,
+        country: nextProps.data.country,
+        postCode: nextProps.data.postCode,
+        state: nextProps.data.state,
+        city: nextProps.data.city,
+        street: nextProps.data.street,
+      })
     }
   }
   onInputChange(e) {
-    this.setState({ test: e.target.value })
+    this.setState({ [e.target.id]: e.target.value })
   }
   componentWillUnmount() {
-    this.props.send(1, { name: this.state.test }) //TODO: refactor
+    this.props.send(1, {
+      name: this.state.name,
+      country: this.state.country,
+      postCode: this.state.postCode,
+      state: this.state.state,
+      city: this.state.city,
+      street: this.state.street,
+    }) //TODO: refactor
   }
   render() {
-    const { test } = this.state
+    const { name, country, postCode, state, city, street } = this.state
     return (
       <div>
-
       {
-        this.props.isLoading ? <p>Is loading...</p> : <input value={test} onChange={this.onInputChange} placeholder={'Input name'}/>
+        this.props.isLoading ? <p>Is loading...</p> : 
+        <div>
+          <h1>Create Main Account</h1>
+          <div>
+            <label htmlFor='name'>Main Account Name</label>
+            <input id='name' value={name} onChange={this.onInputChange} placeholder={'Input Name'}/>
+          </div>
+          <div>
+            <label htmlFor='country'>Country</label>
+            <select 
+            name='country' 
+            id='country'
+            defaultValue={country}>
+              <option value='CZ'>Czech</option>
+              <option value='EN'>Britain</option>
+              <option value='RU'>Russia</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor='post-code'>Post Code</label>
+            <input id='post-code' value={postCode} onChange={this.onInputChange} placeholder={'Input Post Code'}/>
+          </div>
+          <div>
+            <label htmlFor='state'>State</label>
+            <input id='state' value={state} onChange={this.onInputChange} placeholder={'Input State'}/>
+          </div>
+          <div>
+            <label htmlFor='city'>City</label>
+            <input id='city' value={city} onChange={this.onInputChange} placeholder={'Input City'}/>
+          </div>
+          <div>
+            <label htmlFor='street'>Street</label>
+            <input id='street' value={street} onChange={this.onInputChange} placeholder={'Input Street'}/>
+          </div>
+        </div>
       }
-
       </div>
     )
   }
